5340661ae056dc01279a89be3de40dd7c2a40f79
justinenorie <justinedelacruz97@gmail.com>
2024-11-27 21:11:50 +0800

    added a security to hide token

--- models/connection.php
+++ models/connection.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 //Database Connection to configure
 function getMongoClient() {
     static $client = null;
@@ -7,7 +9,7 @@ function getMongoClient() {
     if ($client === null) {
         try {
             //TODO: change the connection into .env
-            $client = new MongoDB\Client("mongodb+srv://easedocu:easedocu123@easecluster.6yvnz.mongodb.net/");
+            $client = new MongoDB\Client($_ENV['MongoDBTOken']);
             // $client = new MongoDB\Client("mongodb://localhost:27017/");
             // echo "Connected to MongoDB successfully.";
         } catch (Exception $e) {
